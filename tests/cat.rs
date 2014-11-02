85000bd2c294abb752345a929904eda0342601ae
Robert Helmer <rhelmer@rhelmer.org>
2014-11-02 12:55:11 -0800

    cat count line test

--- tests/cat.rs
+++ tests/cat.rs
@@ -25,3 +25,16 @@ fn test_cat_file_stdout() {
 
     assert_eq!(output.output.as_slice(), "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n".as_bytes());
 }
+
+#[test]
+fn test_cat_count_file_stdout() {
+    let output = match Command::new("./target/cat")
+                            .arg("-n")
+                            .arg("tests/fixtures/cat/multiline.txt")
+                            .output() {
+        Ok(p) => p,
+        Err(e) => panic!("failed to execute process: {}", e),
+    };
+
+    assert_eq!(output.output.as_slice(), "    1  0123456789\n    2  abcdefghijklmnopqrstuvwxyz\n    3  ABCDEFGHIJKLMNOPQRSTUVWXYZ\n".as_bytes());
+}
