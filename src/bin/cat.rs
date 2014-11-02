11c373ee8157101206a3658115cb8bb8c2502c4a
Robert Helmer <rhelmer@rhelmer.org>
2014-11-02 12:07:41 -0800

    fail! renamed to panic! in rust

--- src/bin/cat.rs
+++ src/bin/cat.rs
@@ -56,7 +56,7 @@ fn main() {
 
     let matches = match getopts(args.tail(), opts) {
         Ok(m) => { m }
-        Err(f) => { fail!(f.to_string()) }
+        Err(f) => { panic!(f.to_string()) }
     };
 
     let files = matches.free.clone();
